dad3fa13d09ed49c094fd05e904ece230207f0b0
luis <luislopez0690@gmail.com>
2018-03-06 17:54:51 -0500

    login partially working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,51 +14,45 @@ class UsersController < ApplicationController
     render json: @user
   end
 
-  # GET /user
-  def get_user_by_name
-    if params[:name]
-      user = User.where(name: params[:name]).first
-    elsif params[:password]
-      user = User.where(password: params[:password]).first
-    render json: user
-  else
-    render json: user.errors, status: unprocessable_entity
-  end
-  end
-
-
   # POST /users
-  def create
-    if !User.where(name: user_params[:name])
-      @user = User.new(user_params)
+  # def create
+  #   if !User.where(name: user_params[:name])
+  #     @user = User.new(user_params)
+  #     if @user.save
+  #     render json: @user, status: :created, location: @user
+  #     else
+  #     render json:{errors:["Failed to save"]}, status:500
+  #     end
+  #   elsif (User.where(name: user_params[:name])).any? && (User.where(password: user_params[:password])).any?
+  #     puts (User.where(name: user_params[:name])).any?
+  #     puts (User.where(password: user_params[:password])).any?
+  #     puts (User.where(name: user_params[:name])).any? && (User.where(password: user_params[:password])).any?
+  #     @user = User.find_by(name: user_params[:name])
+  #     render json: @user, status: :created
+  #   else
+  #     render json:{errors:["Username already, try logging in instead"]}, status:401
+  #   end
+  # end
+
+  def signup
+
+    if !User.where(name: params[:name]).any?
+      @user = User.create({
+        :name => params[:name],
+        :password => params[:password]
+        })
       if @user.save
       render json: @user, status: :created, location: @user
       else
       render json:{errors:["Failed to save"]}, status:500
       end
-    elsif (User.where(name: user_params[:name])).any? && (User.where(password: user_params[:password])).any?
-      puts (User.where(name: user_params[:name])).any?
-      puts (User.where(password: user_params[:password])).any?
-      puts (User.where(name: user_params[:name])).any? && (User.where(password: user_params[:password])).any?
-      @user = User.find_by(name: user_params[:name])
-      render json: @user, status: :created
     else
       render json:{errors:["Username already, try logging in instead"]}, status:401
     end
   end
 
-  def signup
-    puts "HOLA SIGNUP"
-    if !User.where(name: params[:name])
-      @user = User.new(params)
-      if @user.save
-      render json: @user, status: :created, location: @user
-      else
-      render json:{errors:["Failed to save"]}, status:500
-      end
-    elsif (User.where(name: params[:name])).any? && (User.where(password: params[:password])).any?
-      puts (User.where(name: params[:name])).any?
-      puts (User.where(password: params[:password])).any?
+  def login
+    if (User.where(name: params[:name])).any? && (User.where(password: params[:password])).any?
       puts (User.where(name: params[:name])).any? && (User.where(password: params[:password])).any?
       @user = User.find_by(name: params[:name])
       render json: @user, status: :created
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :sessions
 
   post "users/signup" => 'users#signup'
-
+  post "users/login" => 'users#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
