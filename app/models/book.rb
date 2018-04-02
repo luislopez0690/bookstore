65e23f93fcdb5ac3f65c34293b4865ce6cb56f5a
luis <luislopez0690@gmail.com>
2018-04-02 18:18:35 -0400

    f

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   has_many :users, through: :transactions
 
   def available_books
+    
     total_bought_books = self.transactions.sum(:quantity)
     self.available = self.amount - total_bought_books
     self.save
