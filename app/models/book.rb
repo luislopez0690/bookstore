c5e4f03e36210c7558e3978a013995dade7c2b45
luis <luislopez0690@gmail.com>
2018-04-02 18:20:20 -0400

    new stuff

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ApplicationRecord
   has_many :transactions
   has_many :users, through: :transactions
-<<<<<<< HEAD
 
   def available_books
     
@@ -9,6 +8,4 @@ class Book < ApplicationRecord
     self.available = self.amount - total_bought_books
     self.save
   end
-=======
->>>>>>> e421485dad613cc8ad215f360a6f4addb059b5b9
 end
